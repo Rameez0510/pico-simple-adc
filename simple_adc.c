ed6098409baea07a7d9b2c5133c2b84f14a744b5
Rameez0510 <khanrameezrk428@gmail.com>
2026-07-30 18:18:10 +0530

    First commit

---
+++ simple_adc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/adc.h"
+#include "max7219.h"
+
+
+int main()
+{
+    stdio_init_all();
+
+    while (true) {
+
+    }
+}
